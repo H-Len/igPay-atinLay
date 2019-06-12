123eb3c524a96f4c0d4e2eb90a910061a0f00f1d
hannah melendy and hugh sherman donkin <hannah@>
2019-06-12 16:43:51 -0700

    add-way ully-fay unctional-fay ogic-lay o-tay js-ay; add a list of bugs on README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,96 @@
 $(document).ready(function(){
   console.log("Jquery works!");
 
-    var inputWordArray = [];
-    var inputWord;
 
+  // var favoritePets = ["cats", "dogs", "owls"];
+  // var favoriteFish = ["goldfish", "bluefish"];
+  // var newArray = [];
+  // newArray.push(favoritePets);
+  // newArray.push(favoriteFish);
+  // console.log(newArray);
+  //
+  //
+
+
+
+
+
+
+
+    var fullSentenceArray =[];
+    var workingWordArray = [];
+    var inputSentence;
+    var splitArray = [];
+    var loopArray = [];
+    var finalSentence = "";
 
   function gatherInput(){
-    inputWord = $("#input-word").val();
+    inputSentence = $("#input-word").val();
   }
 
-  function arrayMaker() {
-    inputWordArray = Array.from(inputWord);
+
+  function arrayMaker(input) {
+    return Array.from(input);
   }
 
+  // function firstVowelTester(){
+  //   if (splitArray[i] === "a" || splitArray[i] === "e" || splitArray[i] === "i" || splitArray[i] === "o" || splitArray[i] === "u") {
+  //     return splitArray[i] + "-way";
+  //   }
+  // }
+
+  // this is the original firstVowel Tester
   function firstVowelTester(){
-    if (inputWordArray[0] === "a" || inputWordArray[0] === "e" || inputWordArray[0] === "i" || inputWordArray[0] === "o" || inputWordArray[0] === "u") {
-      return inputWord + "-way";
+    if (workingWordArray[0] === "a" || workingWordArray[0] === "e" || workingWordArray[0] === "i" || workingWordArray[0] === "o" || workingWordArray[0] === "u") {
+      workingWordArray.push("-way");
     }
   }
 
   function firstConsonantTester() {
     // this checks if the first two letters of the word are qu or QU
-    if(inputWord[0] === "q" && inputWord[1] === "u" || inputWordArray[0] === "Q" && inputWordArray[1] === "U"){
-      var firstQAndU = (inputWordArray.shift())+ (inputWordArray.shift());
+    if(inputSentence[0] === "q" && inputSentence[1] === "u" || workingWordArray[0] === "Q" && workingWordArray[1] === "U"){
+      var firstQAndU = (workingWordArray.shift())+ (workingWordArray.shift());
       console.log("Here is the qu:" + firstQAndU);
-      inputWordArray.push("-" + firstQAndU + "ay");
+      workingWordArray.push("-" + firstQAndU + "ay");
+    }
+    if (workingWordArray[0] != "a" && workingWordArray[0] != "e" && workingWordArray[0] != "i" && workingWordArray[0] != "o" && workingWordArray[0] != "u") {
 
-    } else if (inputWordArray[0] != "a" || inputWordArray[0] != "e" || inputWordArray[0] != "i" || inputWordArray[0] != "o" || inputWordArray[0] != "u") {
-
-    var firstConsonant = inputWordArray.shift();
-    console.log(inputWordArray);
+    var firstConsonant = workingWordArray.shift();
+    console.log(workingWordArray);
     console.log(firstConsonant);
-    inputWordArray.push("-" + firstConsonant + "ay");
+    workingWordArray.push("-" + firstConsonant + "ay");
     }
   }
 
-
-
-  function pigLatinMaker() {
-    firstVowelTester();
-    firstConsonantTester();
-     var pigLatinPhrase = inputWordArray.join("");
-     console.log(pigLatinPhrase);
+  function sentenceSplitter (input) {
+    //splitting input into separate words
+    return input.split(" ");
   }
 
 
+  function pigLatinMaker() {
+    gatherInput();
+    console.log(inputSentence);
+    fullSentenceArray = sentenceSplitter(inputSentence);
+    console.log(fullSentenceArray);
+
+    for (i = 0; i < fullSentenceArray.length; i++){
+      workingWordArray = arrayMaker(fullSentenceArray[i]);
+      console.log(workingWordArray);
+      firstVowelTester();
+      firstConsonantTester();
+      var pigLatinWord = workingWordArray.join("");
+      finalSentence = finalSentence + " " + pigLatinWord;
+    }
+
+     console.log(finalSentence);
+  }
 
 
   // this is the UI logic
   $("#pig-latin-form").submit(function(event){
     event.preventDefault();
-    gatherInput();
-    arrayMaker();
+
     pigLatinMaker();
 
 
